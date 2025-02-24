8242788cffe6b66e501991f45da947e6a84dc881
Preston Huft <phuft@quera.com>
2025-02-24 09:49:48 -0500

    organize units and constants

--- utils/units.py
+++ utils/units.py
@@ -1,13 +1,26 @@
+# time
 s = 1
 ms = 1e-3
 us = 1e-6
+
+# length
 m = 1
 cm = 1e-2
 mm = 1e-3
 um = 1e-6
 nm = 1e-9
+
+# rate
 Hz = 1
 kHz = 1e3
 MHz = 1e6
+
+# angle
 rad = 1
 mrad = 1e-3
+
+# power
+W = 1
+mW = 1e-3
+uW = 1e-6
+
